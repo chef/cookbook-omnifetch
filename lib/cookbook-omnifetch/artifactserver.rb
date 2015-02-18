81184fc125855f40567a0d0176ed67ef721420fd
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-02-18 12:20:44 -0500

    Change artifact server to use a staging directory that is adjacent to the cache directory.

--- lib/cookbook-omnifetch/artifactserver.rb
+++ lib/cookbook-omnifetch/artifactserver.rb
@@ -39,14 +39,21 @@ module CookbookOmnifetch
     # @return [void]
     def install
       FileUtils.mkdir_p(cache_root) unless cache_root.exist?
+      FileUtils.mkdir_p(staging_root) unless staging_root.exist?
 
-      http = http_client(uri)
-      http.streaming_request(nil) do |tempfile|
-        tempfile.close
-        FileUtils.mv(tempfile.path, cache_path)
-      end
+      Dir.mktmpdir(nil, staging_root) do |staging_dir|
+        http = http_client(uri)
+        http.streaming_request(nil) do |tempfile|
+          tempfile.close
+          # TODO: What are the ACLs supposed to be?  Windows might or might not
+          # recalculate your ACLs based on how the move was accomplished.
+          staging_tgz_path = File.join(staging_dir, "#{cache_key}.tgz")
+          # Might be atomic?
+          FileUtils.mv(tempfile.path, staging_tgz_path)
+          # Probably is atomic.
+          FileUtils.mv(staging_tgz_path, cache_path)
+        end
 
-      Dir.mktmpdir do |staging_dir|
         Zlib::GzipReader.open(cache_path) do |gz_file|
           tar = Archive::Tar::Minitar::Input.new(gz_file)
           tar.each do |e|
@@ -114,8 +121,26 @@ module CookbookOmnifetch
       other.rel == rel
     end
 
+    # The path where all pristine tarballs from an artifactserver are held.
+    # Tarballs are moved/swapped into this location once they have been staged
+    # in a co-located staging directory.
+    #
+    # @return [Pathname]
     def cache_root
       Pathname.new(CookbookOmnifetch.cache_path).join('.cache', 'artifactserver')
+    end
+
+    # The path where tarballs are downloaded to and unzipped.  On certain platforms
+    # *cough* Windows *cough*, you have a better chance of getting an atomic move
+    # if your temporary working directory is on the same device/volume as the
+    # destination.  To support this, we use a staging directory located under the
+    # cache path under the rather mild assumption that everything under the cache
+    # path is going to be on one device.  Do not create anything under this directory
+    # that isn't randomly named and remember to release your files once you are done.
+    #
+    # @return [Pathname]
+    def staging_root
+      Pathname.new(CookbookOmnifetch.cache_path).join('.cache_tmp', 'artifactserver')
     end
 
     # The path where the pristine tarball is cached
